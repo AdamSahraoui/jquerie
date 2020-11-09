32901e90409078313c6eaac94fbbadb89a10bb86
Adam Sahraoui <adamsahraoui@MacBook-Pro-de-Adam.local>
2020-11-09 10:26:31 +0100

    partie2

--- Exercice 5/assets/js/main6.js
+++ Exercice 5/assets/js/main6.js
@@ -1,1 +1,8 @@
+$(document).ready(function(){ 
 $('li').css('color','red');
+});
+
+//premier ex marche mais le deuxieme correspond a l'intitulé exact de l'exo
+$(document).ready(function(){
+$("ul").children().css("color","red");
+});
---
+++ exo1p2/assets/js/main7.js
@@ -0,0 +1,7 @@
+$(document).ready(function() { 
+
+    $("#image").mouseover(function(){
+    $("#image").css("width","+=10%");  
+        
+    })
+ })
