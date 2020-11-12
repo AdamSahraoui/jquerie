793d3171ee4d0dd10e73af16ddf98f78d527cd5a
Adam Sahraoui <adamsahraoui@MacBook-Pro-de-Adam.local>
2020-11-12 10:32:05 +0100

    exojusteprix

---
+++ lejusteprix/assets/maintp.js
@@ -0,0 +1,24 @@
+var nb_coup = 0;
+	var nb = Math.floor(100*Math.random());
+	
+	//alert(nb); // pour voir la solution
+	
+	function jeux (){
+		var nombre = document.getElementById('solution').value;
+		
+		nb_coup++;
+		
+		if(nombre < nb) { // c'est plus 
+			alert("c'est plus");
+			document.getElementById('solution').value = '';
+		}
+		if(nombre > nb) { // c'est moins
+			alert("c'est moins");
+			document.getElementById('solution').value = '';
+		}
+		if(nombre == nb) { // La personne a gagné 
+			alert("BRAVO , le nombre était "+ nb + "." + " Tu as gagné en "+ nb_coup +" coups");
+			
+			
+		}
+	}
